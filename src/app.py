d700c158d53a430343581ca250ec7191b6e3662e
Negar Shahrokh <neg.shahrokh@gmail.com>
2024-06-05 10:16:00 -0400

    added the details page

--- src/app.py
+++ src/app.py
@@ -1,7 +1,13 @@
 from flask import Flask, jsonify, render_template
+import socket
 
 ## creating an app
 app = Flask(__name__)
+
+def fetch():
+    host = str(socket.gethostname())
+    ip = str(socket.gethostbyname(host))
+    return host, ip
 
 ## adding the first route
 @app.route("/")
@@ -13,6 +19,14 @@ def hello_world():
 @app.route("/health")
 def health():
     return jsonify(status="UP")
+
+## adding the details path
+## for checking the host
+## and IP of the contianer
+@app.route("/details")
+def details():
+    host, ip = fetch()
+    return render_template("details.html", HOST=host, IP=ip)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
